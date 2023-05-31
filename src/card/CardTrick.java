54c75cf1cf129675d5a23b81b1d92d54a7d7084a
priyankagoyal12 <priyan21@sheridancollege.ca>
2023-05-31 15:51:04 -0400

    Type1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,8 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package card;
-
+ package card;
+ import java.util.Random;
+ import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -19,9 +20,24 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            Random pick= new Random();
+            c.setValue(pick.nextInt(13)+1);//Generates a random value between 1 and 13 (inclusive)
+            c.setSuit(Card.SUITS[pick.nextInt(3)]);//Generates a random index between 0 and 3
+            magicHand[i] =c;
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+        Scanner keyboard = new Scanner(System.in);
+        System.out.println("Enter the value of your card between 1 and 13");
+        int choice=keyboard.nextInt();
+        keyboard.nextLine();
+        System.out.println("Enter the valid suit of your card");
+        String suit=keyboard.nextLine();
+        
+        Card customerCard= new Card();
+        customerCard.setValue(choice);
+        customerCard.setSuit(suit);
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
