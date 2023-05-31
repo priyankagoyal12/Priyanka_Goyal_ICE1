46fd8884a32c7e75a1de9c31ea299c52ffff8871
priyankagoyal12 <priyan21@sheridancollege.ca>
2023-05-31 17:04:09 -0400

    Type1

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -9,7 +9,8 @@
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
- * @author srinivsi
+ * @author priyanka
+ * //Priyanka Priyanka (991676014) 
  */
 public class CardTrick {
     
@@ -28,15 +29,14 @@ public class CardTrick {
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
         Scanner keyboard = new Scanner(System.in);
-        System.out.println("Enter the value of your card between 1 and 13");
+        System.out.println("Enter the value of your card:");
         int choice=keyboard.nextInt();
-        keyboard.nextLine();
-        System.out.println("Enter the valid suit of your card");
-        String suit=keyboard.nextLine();
+        System.out.println("Enter the valid suit of your card(0-3)from Hearts,Diamonds,Spades,Clubs: ");
+        int suit=keyboard.nextInt();
         
         Card customerCard= new Card();
         customerCard.setValue(choice);
-        customerCard.setSuit(suit);
+        customerCard.setSuit(Card.SUITS[suit]);
         
          boolean foundMatch = false;
         for (Card card : magicHand) {
@@ -50,14 +50,10 @@ public class CardTrick {
             System.out.println("Congratulations! Your card was found in the magic hand.");
         } else {
             System.out.println("Sorry, your card was not found in the magic hand.");
-        Card luckyCard = new Card();
-        luckyCard.setValue(10);  // Set the card number of your choosing
-        luckyCard.setSuit("Hearts");
+        
         }
     }
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
     }
-    
-
